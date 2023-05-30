e3493ae1eebaf9d9821668a935da6bf9b27a6605
Занин Даниил <etu.zanin@yandex.ru>
2023-05-31 01:51:49 +0300

    Внесены изменения в файл requirements.sh
    Добавили автозагрузку nslookup

--- requirements.sh
+++ requirements.sh
@@ -1,6 +1,7 @@
 apt-get -y update
-apt-get install nmap -y
 apt install amass -y
+apt install nslookup -y
+apt-get install nmap -y
 apt install nuclei -y
 apt-get install git -y
 git clone https://github.com/projectdiscovery/nuclei-templates.git
